a1f015b31ddae9980593f34a7fa8249693decd1a
Aram Zucker-Scharff <azuckers@gmu.edu>
2012-07-08 19:50:44 -0400

    implementing gallary shortcode

--- exif-everywhere.php
+++ exif-everywhere.php
@@ -104,7 +104,7 @@ add_action( 'wp_enqueue_scripts', 'style_exif_slider_plugin', 1 );
 	
 	if ($floatWPVer >= 3.4){
 
-		function jq_setup() {
+		function jq_threefour_setup() {
 				
 				wp_enqueue_script('jquery');
 				wp_enqueue_script('jquery-cycle', plugins_url('js/jquery.cycle.all.js', __FILE__), array('jquery'));
@@ -114,7 +114,7 @@ add_action( 'wp_enqueue_scripts', 'style_exif_slider_plugin', 1 );
 
 		}
 
-		add_action('wp_enqueue_scripts', 'jq_setup');
+		add_action('wp_enqueue_scripts', 'jq_threefour_setup');
 	} else {
 			
 		function jq_enqueue() {
@@ -546,6 +546,9 @@ function exif_gallery_shortcode($attr){
 			'autoplay' => 'yes',
 			'title' => 'Gallery',
 			'fontsize' => '1em',
+			'itemtag'    => 'dl',
+			'icontag'    => 'dt',
+			'captiontag' => 'dd',
 			'share' => 'on',
 			'embed' => 'on',
 			'order' => 'ASC',
@@ -577,25 +580,94 @@ function exif_gallery_shortcode($attr){
 				<?php
 				
 					
-				
-					//Using code from http://net.tutsplus.com/tutorials/php/quick-tip-loop-through-folders-with-phps-glob/ to build a quick demo
-					
-					//http://localhost/xampp/wp-test/wp-content/plugins/exif-everywhere/test.php
-				
-					$dir = "testimgs/*";
-					
-						foreach(glob($dir) as $file) {
-						
-							echo '<div class="slide">';
-								echo '<a href="'. $file . '" rel="lightbox"><img src="' . $file . '" /></a>';
-								echo '<div class="exif-data disappear">Some EXIF info</div>';
-								echo '<div class="caption">
-										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec sapien sit amet diam pretium adipiscing. Phasellus euismod mi tincidunt elit mattis sit amet tincidunt est varius. Nunc pretium augue at nibh lacinia sit amet feugiat massa interdum. 
-									</div>
-									<div class="photog">Photog Name</div>
-								</div><!-- end slide -->';
-						
-						}
+
+	$id = intval($id);
+	if ( 'RAND' == $order )
+		$orderby = 'none';
+
+	if ( !empty($include) ) {
+		$include = preg_replace( '/[^0-9,]+/', '', $include );
+		$_attachments = get_posts( array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+
+		$attachments = array();
+		foreach ( $_attachments as $key => $val ) {
+			$attachments[$val->ID] = $_attachments[$key];
+		}
+	} elseif ( !empty($exclude) ) {
+		$exclude = preg_replace( '/[^0-9,]+/', '', $exclude );
+		$attachments = get_children( array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+	} else {
+		$attachments = get_children( array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+	}
+
+	if ( empty($attachments) )
+		return '';
+
+	if ( is_feed() ) {
+		$output = "\n";
+		foreach ( $attachments as $att_id => $attachment )
+			$output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+		return $output;
+	}
+
+	$itemtag = tag_escape($itemtag);
+	$captiontag = tag_escape($captiontag);
+	$columns = intval($columns);
+	$itemwidth = $columns > 0 ? floor(100/$columns) : 100;
+	$float = is_rtl() ? 'right' : 'left';
+
+	$selector = "gallery-{$instance}";
+
+	$gallery_style = $gallery_div = '';
+	if ( apply_filters( 'use_default_gallery_style', true ) )
+		$gallery_style = "
+		<style type='text/css'>
+			#{$selector} {
+				margin: auto;
+			}
+			#{$selector} .gallery-item {
+				float: {$float};
+				margin-top: 10px;
+				text-align: center;
+				width: {$itemwidth}%;
+			}
+			#{$selector} img {
+				border: 2px solid #cfcfcf;
+			}
+			#{$selector} .gallery-caption {
+				margin-left: 0;
+			}
+		</style>
+		<!-- see gallery_shortcode() in wp-includes/media.php -->";
+	$size_class = sanitize_html_class( $size );
+	$gallery_div = "<div id='$selector' class='gallery galleryid-{$id} gallery-columns-{$columns} gallery-size-{$size_class}'>";
+	$output = apply_filters( 'gallery_style', $gallery_style . "\n\t\t" . $gallery_div );
+
+	$i = 0;
+	foreach ( $attachments as $id => $attachment ) {
+		$link = isset($attr['link']) && 'file' == $attr['link'] ? wp_get_attachment_link($id, $size, false, false) : wp_get_attachment_link($id, $size, true, false);
+
+		$output .= "<{$itemtag} class='gallery-item'>";
+		$output .= "
+			<{$icontag} class='gallery-icon'>
+				$link
+			</{$icontag}>";
+		if ( $captiontag && trim($attachment->post_excerpt) ) {
+			$output .= "
+				<{$captiontag} class='wp-caption-text gallery-caption'>
+				" . wptexturize($attachment->post_excerpt) . "
+				</{$captiontag}>";
+		}
+		$output .= "</{$itemtag}>";
+		if ( $columns > 0 && ++$i % $columns == 0 )
+			$output .= '<br style="clear: both" />';
+	}
+
+	$output .= "
+			<br style='clear: both;' />
+		</div>\n";
+
+	return $output;
 					
 				?>
 			
